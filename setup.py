9143c125176ee24bc06f4eb00e86f6a73e58083c
Albert Alonso <alonfnt@pm.me>
2025-01-26 10:26:43 +0100

    fix: add missing dependency to actions

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     include_dirs=[np.get_include()],
     define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     packages=["boundvor"],
+    package_data={
+    "boundvor": ["geometry.pyx"],  # This includes your .pyx file
+},
     extras_require={"dev": ["pytest", "cython"]},
 )
